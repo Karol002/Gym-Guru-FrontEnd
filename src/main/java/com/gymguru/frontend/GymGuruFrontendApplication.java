78a44a0fda1415f82e5ef0290e3bfac6410610b5
Karol002 <karol.wojcik.002@gmail.com>
2023-03-29 15:50:53 +0200

    First commit

---
+++ src/main/java/com/gymguru/frontend/GymGuruFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.gymguru.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymGuruFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymGuruFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gymguru/frontend/GymGuruFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymguru.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymGuruFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
